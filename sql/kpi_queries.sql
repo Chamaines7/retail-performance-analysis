dfcc34562f5acff71e326f283453ae808df5ea9d
Chamaines7 <chamaines7@gmail.com>
2026-01-29 17:41:47 -0500

    Update kpi_queries.sql

--- sql/kpi_queries.sql
+++ sql/kpi_queries.sql
@@ -10,10 +10,26 @@ GROUP BY s.store_id, s.store_name
 ORDER BY total_revenue DESC;
 
 -- Units per Transaction
-
+SELECT 
+	s.store_name,
+	SUM(t.quantity) / COUNT(DISTINCT t.transaction_id)AS upt
+FROM transactions t
+JOIN stores s ON t.store_id = s.store_id
+GROUP BY s.store_name
+ORDER BY s.store_name ASC;
 
 -- Average Ticket Value
-
+SELECT 
+	s.store_name,
+	ROUND(
+	SUM(t.quantity *p.price) / COUNT(DISTINCT t.transaction_id),
+	2) 
+	AS average_ticket_value
+FROM transactions t
+JOIN products p ON t.product_id = p.product_id
+JOIN stores s ON t.store_id = s.store_id
+GROUP BY s.store_name
+ORDER BY average_ticket_value DESC;
 
 -- Labor Cost %
 SELECT
