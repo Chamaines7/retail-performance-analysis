6d36f40eb5791b97232a35917628c03d5e5a91c1
Chamaines7 <chamaines7@gmail.com>
2026-01-29 19:17:53 -0500

    Update kpi_queries.sql

--- sql/kpi_queries.sql
+++ sql/kpi_queries.sql
@@ -31,20 +31,6 @@ JOIN stores s ON t.store_id = s.store_id
 GROUP BY s.store_name
 ORDER BY average_ticket_value DESC;
 
--- Labor Cost %
-SELECT
-    s.store_name,
-    ROUND(
-        SUM(l.labor_cost) / SUM(t.quantity * p.price) * 100,
-        2
-    ) AS labor_pct
-FROM labor l
-JOIN stores s ON l.store_id = s.store_id
-JOIN transactions t ON s.store_id = t.store_id
-JOIN products p ON t.product_id = p.product_id
-GROUP BY s.store_name
-ORDER BY labor_pct DESC;
-
 -- Store vs Goal
 SELECT
     s.store_name,
@@ -57,13 +43,5 @@ JOIN stores s ON t.store_id = s.store_id
 GROUP BY s.store_name, s.monthly_sales_goal
 ORDER BY variance DESC;
 
--- Week-over-Week Trends
-SELECT
-    DATE_TRUNC('week', t.transaction_date) AS week,
-    SUM(t.quantity * p.price) AS weekly_revenue
-FROM transactions t
-JOIN products p ON t.product_id = p.product_id
-GROUP BY week
-ORDER BY week;
 
 
